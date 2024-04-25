4af80104e4eb866faae19a165079a01802015bf7
SantiagoV0106 <velascosantiago3@gmail.com>
2024-04-24 19:50:27 -0500

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+// Data
+import headerImg from '/id.svg'
+
+// Style
+import './header.css'
+
+export function Header() {
+    return(
+        <header>
+            <img src={headerImg} alt="Header-Img" />
+        </header>
+    )
+}
