0a63b1b19b65ad29d0fe22643c0c549093c3ab1a
SantiagoV0106 <velascosantiago3@gmail.com>
2024-04-25 19:30:56 -0500

    import image

--- src/const/projects.js
+++ src/const/projects.js
@@ -1,39 +1,47 @@
+import nexus from '/src/assets/images/projects/nexus.jpg'
+import redbull from '/src/assets/images/projects/redbull.jpg'
+import hp from '/src/assets/images/projects/hp.jpg'
+import oncheck from '/src/assets/images/projects/oncheck.jpg'
+import mp from '/src/assets/images/projects/mp.jpg'
+import enerbit from '/src/assets/images/projects/enerbit.png'
+
+
 export const projects = [
     {
         id : crypto.randomUUID(),
         link : 'https://www.behance.net/gallery/158336877/NEXUS-UI-PROYECT',
-        img : '../src/assets/images/projects/nexus.jpg',
+        img : nexus,
         name : 'Nexus'
     },
 
     {
         id : crypto.randomUUID(),
         link : 'https://www.behance.net/gallery/163438601/Red-Bull-OOH-Experience',
-        img : '../src/assets/images/projects/redbull.jpg',
+        img : redbull,
         name : 'RedBull'
     },
     {
         id : crypto.randomUUID(),
         link : 'https://www.behance.net/gallery/145695963/Hospital-Padrino-UIUX-Project',
-        img : '../src/assets/images/projects/hp.jpg',
+        img : hp,
         name : 'Hospital Padrino'
     },
     {
         id : crypto.randomUUID(),
         link : 'https://www.behance.net/gallery/155575969/On-Check-UXUI-PROJECT',
-        img : '../src/assets/images/projects/oncheck.jpg',
+        img : oncheck,
         name : 'onCheck'
     },
     {
         id : crypto.randomUUID(),
         link : 'https://www.behance.net/gallery/172658785/Marketplace-UXUI-Project',
-        img : '../src/assets/images/projects/mp.jpg',
+        img : mp,
         name : 'MarketPlacee'
     },
     {
         id : crypto.randomUUID(),
         link : 'https://www.behance.net/gallery/185492437/enerBit-UIUX-Project',
-        img : '../src/assets/images/projects/enerbit.png',
+        img :enerbit ,
         name : 'enerBit'
     },
 ]
